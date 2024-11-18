e01ba8572511e3fef6f19f428e01909dd1668c72
romina227 <rominagaona04@fpuna.edu.py>
2024-11-18 17:45:45 -0300

    Descripción de los cambios realizados

--- python_vercel_task/urls.py
+++ python_vercel_task/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
 ]
 
 # le saque del segundo path 'blog\'
