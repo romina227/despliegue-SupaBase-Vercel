e77c2c7a2b248efc1d160458f3d4b5d75dfe094a
romina227 <rominagaona04@fpuna.edu.py>
2024-11-18 18:35:57 -0300

    Debug = True nuevamente

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['despliegue-supa-base-vercel', '.vercel.app']
+ALLOWED_HOSTS = ['*', '.vercel.app']
 
 
 # Application definition
